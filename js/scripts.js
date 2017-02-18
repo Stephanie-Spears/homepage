66cfdef39b0ca8736e23f63bdd1174c327b7a070
Stephanie Spears <stephanie.spears1@gmail.com>
2017-02-17 21:08:03 -0800

    add menu icon transformation, add icon change on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,33 @@
 
 
 
-$(document).ready(function(){
-	$('#nav-icon2').click(function(){
-		$(this).toggleClass('open');
-	});
-});
+// $(document).ready(function(){
+//
+//   document.querySelector( "#nav-toggle" ).addEventListener( "click", function() {
+//     this.classList.toggle( "active" );
+//   });
+// });
+var iconClickCounter = 0;
+function changeIcon(){
+  var newIcon;
+  if (iconClickCounter === 0){
+    newIcon = document.getElementById("brandIcon");
+    newIcon.src = "img/mukku.png";
+  }
+  else if (iconClickCounter === 1){
+    newIcon = document.getElementById("brandIcon");
+    newIcon.src = "img/gachapin2.png";
+  }
+  else if (iconClickCounter === 2){
+    newIcon = document.getElementById("brandIcon");
+    newIcon.src = "img/gachapinMukku.png";
+  }
+  else if (iconClickCounter === 3){
+    newIcon = document.getElementById("brandIcon");
+    newIcon.src = "img/gachapin1.png";
+  }
+  else {
+    iconClickCounter = -1;
+  }
+  iconClickCounter += 1;
+}
