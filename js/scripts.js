d308763b9c6d5fb7b0b48551c733ac1a7d452de6
Stephanie Spears <stephanie.spears1@gmail.com>
2017-02-19 02:31:24 -0800

    update brand animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,3 @@
-$.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-    }
-});
-
-
 function changeIcon(){
   var newIcon = document.getElementById("brandIcon");
   var iconArray = ["img/mukku.png", "img/gachapin2.png", "img/gachapinMukku.png", "img/gachapin1.png"];
@@ -20,37 +10,26 @@ function changeIcon(){
   clickCount++;
 }
 
-
 var clickCount = 0;
 $(document).ready(function(){
   $("#nav-icon").click(function(){
     $(this).toggleClass("open");
   });
   $("#brandIcon").click(function(){
-    $("#brandIcon").removeClass("rollIn");
-    $("#brandIcon").removeClass("flipInX");
-    $("#brandIcon").removeClass("rotateInUpLeft");
-    $("#brandIcon").removeClass("bounceInRight");
-    $("#brandIcon").removeClass("hinge");
+    $("#brandIcon").removeClass("bounceIn rollIn flipInX rotateInUpLeft bounceInLeft fadeInRightBig");
     if (clickCount === 0){
       $("#brandIcon").addClass("rollIn");
-
     } else if (clickCount === 1){
       $("#brandIcon").addClass("flipInX");
     } else if (clickCount === 2){
       $("#brandIcon").addClass("rotateInUpLeft");
     } else if (clickCount === 3){
-      $("#brandIcon").addClass("bounceInRight");
+      $("#brandIcon").addClass("bounceInLeft");
+    } else if (clickCount === 4){
+      $("#brandIcon").addClass("fadeInRightBig");
     }
     changeIcon();
-    // $("#brandIcon").removeClass("rollIn");
-    // changeIcon();
-    // $("#brandIcon").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', changeIcon());
-    // $(this).addClass("hinge");
-    // $("#brandIcon").toggleClass("hinge");
 
-    // $(this).toggleClass("rollIn");
-    // $(this).toggleClass("bounceOut");
   });
 
 
