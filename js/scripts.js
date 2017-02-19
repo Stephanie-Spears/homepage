b839640db309d42f55796f6a4bcf95970ddf7ad9
Stephanie Spears <stephanie.spears1@gmail.com>
2017-02-18 23:27:02 -0800

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,34 @@
 var iconClickCounter = 0;
-function changeIcon(){
-  var newIcon = document.getElementById("brandIcon");
-  if (iconClickCounter === 0){
-    newIcon.src = "img/mukku.png";
-  } else if (iconClickCounter === 1){
-    newIcon.src = "img/gachapin2.png";
-  } else if (iconClickCounter === 2){
-    newIcon.src = "img/gachapinMukku.png";
-  } else if (iconClickCounter === 3){
-    newIcon.src = "img/gachapin1.png";
-  }
-   else {
-    iconClickCounter = 0;
-  }
-  iconClickCounter += 1;
-}
 
-var iconArray = ["img/gachapin1.png", "img/mukku.png", "img/gachapin2.png", "img/gachapinMukku.png"];
+var changeIcon = function(){
+  var i = 0;
+  var iconArray = ["img/mukku.png", "img/gachapin2.png", "img/gachapinMukku.png", "img/gachapin1.png"];
+  var newIcon = document.getElementById("brandIcon");
+  newIcon.src = iconArray[i];
+  // if (iconClickCounter === 0){
+  //   newIcon.src = iconArray[0];
+  // } else if (iconClickCounter === 1){
+  //   newIcon.src = iconArray[1];
+  // } else if (iconClickCounter === 2){
+  //   newIcon.src = iconArray[2];
+  // } else if (iconClickCounter === 3){
+  //   newIcon.src = iconArray[3];
+  // } else {
+  //   // newIcon.className += " animated bounceIn";
+  //   iconClickCounter = 0;
+  // }
+  // return iconClickCounter += 1;
+};
+
+
+$(document).ready(function(){
+  $("#nav-icon").click(function(){
+    $(this).toggleClass("open");
+  });
+  $("#brandIcon").click(function(){
+    changeIcon();
+    // $(this).addClass("bounceOut");
+
+  });
+
+});
