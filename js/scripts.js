20c910a24e17ceb0bf2fd85dc5d170d32d308060
Stephanie Spears <stephanie.spears1@gmail.com>
2017-02-19 02:23:44 -0800

    update brand animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,57 @@
-var iconClickCounter = 0;
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
 
-var changeIcon = function(){
-  var i = 0;
-  var iconArray = ["img/mukku.png", "img/gachapin2.png", "img/gachapinMukku.png", "img/gachapin1.png"];
+
+function changeIcon(){
   var newIcon = document.getElementById("brandIcon");
-  newIcon.src = iconArray[i];
-  // if (iconClickCounter === 0){
-  //   newIcon.src = iconArray[0];
-  // } else if (iconClickCounter === 1){
-  //   newIcon.src = iconArray[1];
-  // } else if (iconClickCounter === 2){
-  //   newIcon.src = iconArray[2];
-  // } else if (iconClickCounter === 3){
-  //   newIcon.src = iconArray[3];
-  // } else {
-  //   // newIcon.className += " animated bounceIn";
-  //   iconClickCounter = 0;
-  // }
-  // return iconClickCounter += 1;
-};
+  var iconArray = ["img/mukku.png", "img/gachapin2.png", "img/gachapinMukku.png", "img/gachapin1.png"];
+  if (clickCount < iconArray.length){
+    newIcon.src = iconArray[clickCount];
+  } else {
+    clickCount = 0;
+    newIcon.src = iconArray[clickCount];
+  }
+  clickCount++;
+}
 
 
+var clickCount = 0;
 $(document).ready(function(){
   $("#nav-icon").click(function(){
     $(this).toggleClass("open");
   });
   $("#brandIcon").click(function(){
-    changeIcon();
-    // $(this).addClass("bounceOut");
+    $("#brandIcon").removeClass("rollIn");
+    $("#brandIcon").removeClass("flipInX");
+    $("#brandIcon").removeClass("rotateInUpLeft");
+    $("#brandIcon").removeClass("bounceInRight");
+    $("#brandIcon").removeClass("hinge");
+    if (clickCount === 0){
+      $("#brandIcon").addClass("rollIn");
 
+    } else if (clickCount === 1){
+      $("#brandIcon").addClass("flipInX");
+    } else if (clickCount === 2){
+      $("#brandIcon").addClass("rotateInUpLeft");
+    } else if (clickCount === 3){
+      $("#brandIcon").addClass("bounceInRight");
+    }
+    changeIcon();
+    // $("#brandIcon").removeClass("rollIn");
+    // changeIcon();
+    // $("#brandIcon").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', changeIcon());
+    // $(this).addClass("hinge");
+    // $("#brandIcon").toggleClass("hinge");
+
+    // $(this).toggleClass("rollIn");
+    // $(this).toggleClass("bounceOut");
   });
+
 
 });
