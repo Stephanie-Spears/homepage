26dfad98085c1d1f609deb432d614a29d1b471e3
Stephanie Spears <stephanie.spears1@gmail.com>
2017-02-21 18:41:37 -0800

    background gallery upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+var i = 0;
+
+
+
+
+
 function changeIcon(clickCount){
    var newIcon = document.getElementById("brand-icon");
   var iconArray = ["img/mukku.png", "img/gachapin2.png", "img/gachapinMukku.png", "img/gachapin1.png"];
@@ -15,23 +21,18 @@ function changeIcon(clickCount){
 }
 
 function rotateGallery(){
-  var i = 0;
-  var gallery = document.getElementById("homepage-main");
-  var gallerySource = ["background-image:url('img/Arc1.jpg')", "background-image:url('img/Arc2.jpg')", "background-image:url('img/gold.jpg')", "background-image:url('img/Forest.jpg')"];
-  if (i < gallerySource.length){
-    gallery.style = gallerySource[i];
-    alert(gallery.style);
-    i++;
-  }
-  else {
-    i = 0;
-  }
+  var gallerySource = ["img/Arc1.jpg", "img/Arc2.jpg", "img/Juso.jpg", "img/Forest.jpg"];
+  var index = i++ % gallerySource.length;
+
+
+      $("#homepage-main").css("background-image","url('"+ gallerySource[index] +"')");
   // setTimeout(rotateGallery, 3 * 1000);
 }
 // window.onload = rotateGallery;
 
 $(document).ready(function(){
-  rotateGallery();
+  setInterval(rotateGallery, 15000);
+
   $("#nav-icon").click(function(){
     $(this).toggleClass("open");
   });
