5d40858b47bd5c6aa3d267d5ec373b1a6363aab7
Stephanie Spears <stephanie.spears1@gmail.com>
2017-02-23 17:17:12 -0800

    update js functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,41 @@
 var myHomepage = function(){
   //"private" variables:
-  var i = 0;
   var clickCount = 0;
   //"private" methods:
   var setBackground = function(){
+    var i = 0;
     function rotateGallery(){
-      //infinite i count;
-
-      var gallerySource = ["url('img/Arc1.jpg')", "url('img/Arc2.jpg'", "url('img/Juso.jpg'", "url('img/Forest.jpg'"];
+      //BUG: infinite i count;
+      var gallerySource = ["url(img/Arc1.jpg)", "url(img/Arc2.jpg)", "url(img/Juso.jpg)", "url(img/Forest.jpg)"];
       var index = i++ % gallerySource.length;
 
-      document.getElementById("homepage-main").style.backgroundImage = gallerySource[index];
+      $("#homepage-main").css("background-image", gallerySource[index]);
+      // document.getElementById("homepage-main").style.backgroundImage = gallerySource[index];
     }
-    setInterval(function(){rotateGallery();},25000);
+    var galleryInterval = setInterval(function(){rotateGallery();},25000);
   };
-  setBackground();
 
-  $("#nav-icon").click(function(){
+
+  function changeIcon(){
+    var newIcon = document.getElementById("brand-icon");
+    var iconArray = ["img/mukku.png", "img/gachapin2.png", "img/gachapinMukku.png", "img/gachapin1.png"];
+    var animationArray = [" rollIn", " flipInX", " rotateIn", " rubberBand"];
+    if (clickCount < iconArray.length){
+      newIcon.src = iconArray[clickCount];
+      newIcon.className += animationArray[clickCount];
+    } else {
+      clickCount = 0;
+      newIcon.src = iconArray[clickCount];
+      newIcon.className += animationArray[clickCount];
+    }
+    clickCount++;
+    return clickCount;
+  }
+
+
+  $(document).ready(function(){
+    setBackground();
+    $("#nav-icon").click(function(){
       $(this).toggleClass("open");
     });
     $("#brand-icon").click(function(){
@@ -27,27 +46,12 @@ var myHomepage = function(){
       $("html,body").animate({
         scrollTop: $("#resume-main").offset().top},"slow");
       });
+    });
+
 
 }(); // the parens here cause the anonymous function to execute and return
 
 
-// var i = 0;
-
-// function changeIcon(clickCount){
-//    var newIcon = document.getElementById("brand-icon");
-//   var iconArray = ["img/mukku.png", "img/gachapin2.png", "img/gachapinMukku.png", "img/gachapin1.png"];
-//   var animationArray = [" rollIn", " flipInX", " rotateIn", " rubberBand"];
-//   if (clickCount < iconArray.length){
-//     newIcon.src = iconArray[clickCount];
-//     newIcon.className += animationArray[clickCount];
-//   } else {
-//     clickCount = 0;
-//     newIcon.src = iconArray[clickCount];
-//     newIcon.className += animationArray[clickCount];
-//   }
-//   clickCount++;
-//   return clickCount;
-// }
 
 // function rotateGallery(){
 //   var gallerySource = ["img/Arc1.jpg", "img/Arc2.jpg", "img/Juso.jpg", "img/Forest.jpg"];
