0db185e155fbf864f7da68bbc8a9e6af9f920ba1
Stephanie Spears <stephanie.spears1@gmail.com>
2017-02-23 18:43:39 -0800

    remove comments in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 var myHomepage = function(){
-  //"private" variables:
+  //"private" variables
   var clickCount = 0;
-  //"private" methods:
+
+  //"private" methods
   var setBackground = function(){
     var i = 0;
     function rotateGallery(){
-      //BUG: infinite i count;
       var gallerySource = ["url(img/Arc1.jpg)", "url(img/Arc2.jpg)", "url(img/Juso.jpg)", "url(img/Forest.jpg)"];
-      // var index = i++ % gallerySource.length;
       if (i < gallerySource.length){
         $("#homepage-main").css("background-image", gallerySource[i]);
       }
@@ -20,8 +19,8 @@ var myHomepage = function(){
     var galleryInterval = setInterval(function(){rotateGallery();},25000);
   };
 
-
-  function changeIcon(){
+  var setBrand = function(){
+    $("#brand-icon").removeClass("bounceIn rollIn flipInX rotateIn rubberBand");
     var newIcon = document.getElementById("brand-icon");
     var iconArray = ["img/mukku.png", "img/gachapin2.png", "img/gachapinMukku.png", "img/gachapin1.png"];
     var animationArray = [" rollIn", " flipInX", " rotateIn", " rubberBand"];
@@ -34,9 +33,7 @@ var myHomepage = function(){
       newIcon.className += animationArray[clickCount];
     }
     clickCount++;
-    // return clickCount;
-  }
-
+  };
 
   $(document).ready(function(){
     setBackground();
@@ -44,47 +41,12 @@ var myHomepage = function(){
       $(this).toggleClass("open");
     });
     $("#brand-icon").click(function(){
-      $(this).removeClass("bounceIn rollIn flipInX rotateIn rubberBand");
-      // clickCount = changeIcon(clickCount);
-      changeIcon();
+      setBrand();
     });
     $("#resume-btn").click(function(){
-      //says top is undefined
       $("html,body").animate({
         scrollTop: $("#resume-main").offset()},"slow");
       });
     });
 
-
 }(); // the parens here cause the anonymous function to execute and return
-
-
-
-// function rotateGallery(){
-//   var gallerySource = ["img/Arc1.jpg", "img/Arc2.jpg", "img/Juso.jpg", "img/Forest.jpg"];
-//   var index = i++ % gallerySource.length;
-//
-//
-//       $("#homepage-main").css("background-image","url('"+ gallerySource[index] +"')");
-//   // setTimeout(rotateGallery, 3 * 1000);
-// }
-// window.onload = rotateGallery;
-
-// $(document).ready(function(){
-//   setInterval(rotateGallery, 15000);
-//
-//   $("#nav-icon").click(function(){
-//     $(this).toggleClass("open");
-//   });
-//   var clickCount = 0;
-//   $("#brand-icon").click(function(){
-//     $(this).removeClass("bounceIn rollIn flipInX rotateIn rubberBand");
-//     clickCount = changeIcon(clickCount);
-//   });
-//   $("#resume-btn").click(function(){
-//     $("html,body").animate({
-//       scrollTop: $("#resume-main").offset().top},"slow");
-//     });
-//
-//
-// });
