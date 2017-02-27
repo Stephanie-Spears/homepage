78a62f22a1679582fa530360d9358a1d87ab21a4
SYLI150u513 <SYLI150u513@ad.pcc.edu>
2017-02-27 10:45:29 -0800

    test

---
+++ README.md
@@ -0,0 +1,42 @@
+# Stephanie Spears Cirriculum Vitae #
+
+#### _Resume Website for Stephanie Spears, February 11th, 2017_
+#### By ***Stephanie Spears***
+
+## Description
+
+_This is a web application for presenting Stephanie Spears' *web development* Curriculum Vitae_
+
+## Specifications
+
+* _This website provides Stephanie Spears'..._
+  * _contact information_
+  * _skills list_
+  * _previous employment information, with easy-to-follow website links for most employers_
+  * _personal GitHub projects, with brief application descriptions and repository links_
+  * _education history_
+  * _an interactive Google Maps link, highlighting previous places of employment_
+
+## Setup/Installation Requirements
+
+* _If you wish to inspect the resume in greater detail, please clone the following repository from GitHub:_
+* `git clone https://github.com/Stephanie-Spears/homepage.git`
+
+## GitHub Pages link
+
+View this site on GitHub Pages ***[here](https://stephanie-spears.github.io/homepage/)***
+
+## Support and contact details
+
+_Please feel free to contact stephanie.spears1@gmail.com for any additional information or assistance._
+
+## Technologies Used
+
+* _Particles.js_
+* _BootStrap_
+
+### License
+
+*This software is licensed under the MIT license.*
+
+Copyright (c) 2017 **_Stephanie Spears_**
---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+var myHomepage = function(){
+  //"private" variables
+  var clickCount = 0;
+
+  //"private" methods
+  var setBackground = function(){
+    var i = 0;
+    function rotateGallery(){
+      var gallerySource = ["url(img/Arc1.jpg)", "url(img/Arc2.jpg)", "url(img/Juso.jpg)", "url(img/Forest.jpg)"];
+      if (i === gallerySource.length){
+        i = 0;
+      }
+      $("#homepage-main").css("background-image", gallerySource[i]).fadeOut(0).fadeIn(1000);
+      i++;
+    }
+    var galleryInterval = setInterval(function(){rotateGallery();},60000);
+  };
+
+  var setBrand = function(){
+    $("#brand-icon").removeClass("bounceIn rollIn flip rotateIn rubberBand");
+    var newIcon = document.getElementById("brand-icon");
+    var iconArray = ["img/mukku.png", "img/gachapin2.png", "img/gachapinMukku.png", "img/gachapin1.png"];
+    var animationArray = [" rollIn", " flip", " rotateIn", " rubberBand"];
+    if (clickCount < iconArray.length){
+      newIcon.src = iconArray[clickCount];
+      newIcon.className += animationArray[clickCount];
+    } else {
+      clickCount = 0;
+      newIcon.src = iconArray[clickCount];
+      newIcon.className += animationArray[clickCount];
+    }
+    clickCount++;
+  };
+
+  setBackground();
+  $("#nav-icon").click(function(){
+    $("#nav-icon").toggleClass("open");
+  });
+  $("#brand-icon").click(function(){
+    setBrand();
+  });
+  $("#cv-btn").click(function(){
+    $("html,body").animate({
+      scrollTop: $("#cv-main").offset().top},"slow");
+    });
+
+}(); // the parens here cause the anonymous function to execute and return
