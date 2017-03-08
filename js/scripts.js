0141f2b955c2db135aba64c6f6633f93c5e31625
Stephanie Spears <stephanie.spears1@gmail.com>
2017-03-07 20:36:26 -0800

    update js function to be more succinct

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,32 @@
 var myHomepage = function(){
   //"private" variables
-  var clickCount = 0;
+  var i = 0;
+
 
   //"private" methods
   var setBackground = function(){
-    var i = 0;
+    var x = 0;
     function rotateGallery(){
       var gallerySource = ["url(img/Arc1.jpg)", "url(img/Arc2.jpg)", "url(img/Juso.jpg)", "url(img/Forest.jpg)"];
-      if (i === gallerySource.length){
-        i = 0;
+      if (x === gallerySource.length){
+        x = 0;
       }
-      $("#homepage-main").css("background-image", gallerySource[i]).fadeOut(0).fadeIn(1000);
+      $("#homepage-main").css("background-image", gallerySource[x]).fadeOut(0).fadeIn(1000);
       i++;
     }
     var galleryInterval = setInterval(function(){rotateGallery();},60000);
   };
 
   var setBrand = function(){
-    $("#brand-icon").removeClass("bounceIn rollIn flip rotateIn rubberBand");
-    var newIcon = document.getElementById("brand-icon");
-    var iconArray = ["img/mukku.png", "img/gachapin2.png", "img/gachapinMukku.png", "img/gachapin1.png"];
-    var animationArray = [" rollIn", " flip", " rotateIn", " rubberBand"];
-    if (clickCount < iconArray.length){
-      newIcon.src = iconArray[clickCount];
-      newIcon.className += animationArray[clickCount];
-    } else {
-      clickCount = 0;
-      newIcon.src = iconArray[clickCount];
-      newIcon.className += animationArray[clickCount];
-    }
-    clickCount++;
+    var iconArray = ["img/gachapin1.png", "img/mukku.png", "img/gachapin2.png", "img/gachapinMukku.png"];
+    var animationArray = [ " rollIn", " flip", " rotateIn", " rubberBand"];
+
+    $("#brand-icon").removeClass(animationArray[i]);
+    i = (i < iconArray.length -1) ? ++i : 0;
+    $("#brand-icon").addClass(animationArray[i]);
+    $("#brand-icon").attr("src", iconArray[i]);
   };
+
 
   setBackground();
   $("#nav-icon").click(function(){
@@ -38,7 +34,7 @@ var myHomepage = function(){
   });
   $("#nav-links").click(function(){
     $("#nav-icon").toggleClass("open");
-  $(this).collapse('hide');
+  $(this).collapse("hide");
   });
 
   $("#brand-icon").click(function(){
@@ -53,11 +49,13 @@ var myHomepage = function(){
 
   $(".show-hide-btn").click(function(event){
     event.preventDefault();
-    //condition("if") ?("then") value-if-true :("else") value-if-false
-    var text = $(".more-content").is(":hidden") ? "Show less (-)" : "Show more (+)";
+
+    var text = ($(".more-content").is(":hidden")) ? "Show less (-)" : "Show more (+)";
     $(".show-hide-btn").html(text);
     $(".more-content").toggle("slow");
   });
 
 
 }(); // the parens here cause the anonymous function to execute and return
+
+//condition("if") ?("then") value-if-true :("else") value-if-false
