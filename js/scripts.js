87dd68140202d6680d3cc458b70f8f629eeb9134
Stephanie Spears <stephanie.spears1@gmail.com>
2017-03-09 23:06:33 -0800

    test scrollTop on mobile for show more/less

--- js/scripts.js
+++ js/scripts.js
@@ -35,10 +35,11 @@ var myHomepage = function(){
     $("html, body").animate({scrollTop: $(link).offset().top}, "slow");
   });
   $(".show-hide-btn").click(function(event) {
+    var position = $(this).position().top;
     var txt = $(this).parent().prev(".more-content").is(":visible") ? "Show more (+)" : "Less (–)";
     $(this).parent().prev(".more-content").toggleClass("visible");
     $(this).html(txt);
-    $("html, body").animate({scrollTop: $(this).offset().top}, "slow");
+    $("html, body").animate({scrollTop: $(this).offset().top - position}, "slow");
     event.preventDefault();
   });
 
