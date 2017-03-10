ddc93b88461959143e580914020599cc7e3fa697
Stephanie Spears <stephanie.spears1@gmail.com>
2017-03-09 21:55:48 -0800

    test

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@
 // particlesJS.load('particles-js', 'particles.json', function() {
 //   console.log('particles.js loaded - callback');
 // });
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
 // */
 //
 // /* Otherwise just put the config content (json): */
