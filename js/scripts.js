fb9776b1e1a205bbe618200cc44eaa33eccb100e
Stephanie Spears <stephanie.spears1@gmail.com>
2017-03-10 20:35:38 -0800

    need to fix desktop / mobile button display

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ var myHomepage = function(){
   //back-end
 
   //"private" variables
+  var viewport = document.querySelector("meta[name=viewport]");
+
   var i = 0;
   var x = 0;
   var gallerySource = ["url(img/Forest.jpg)", "url(img/Arc1.jpg)", "url(img/Arc2.jpg)", "url(img/Juso.jpg)"];
@@ -13,10 +15,18 @@ var myHomepage = function(){
     x = (x < gallerySource.length -1) ? ++x : 0;
     $("#homepage-main").css("background-image", gallerySource[x]).fadeOut(0).fadeIn(1000);
   };
+  var changeViewport = function(){
+    var w = $(document).width();
+    var text = w <= 414 ? "Desktop View" : "Mobile View";
+    $(".desktop-mobile-view").html(text);
+    return text;
+  };
 
 
   //front-end
   setInterval(function(){rotateGallery();},600000);
+  var changeViewportText = changeViewport();
+  console.log(changeViewportText);
   $("#nav-icon").click(function(){
     $("#nav-icon").toggleClass("open");
   });
@@ -35,21 +45,36 @@ var myHomepage = function(){
     $("html, body").animate({scrollTop: $(link).offset().top}, "slow");
   });
   $(".show-hide-btn").click(function(event) {
-    var txt = $(this).parent().prev(".more-content").is(":visible") ? "Show more (+)" : "Less (–)";
+    var moreLessText = $(this).parent().prev(".more-content").is(":visible") ? "Show more (+)" : "Less (–)";
     $(this).parent().prev(".more-content").toggleClass("visible");
-    $(this).html(txt);
+    $(this).html(moreLessText);
     $("html, body").animate({scrollTop: $(this).offset().top - 600}, "slow");
     event.preventDefault();
+    // event.stopPropagation();
   });
+  var w = $(document).width();
   $(".desktop-mobile-view").click(function(){
-    var w = ($(document).width());
-    var viewport = document.querySelector("meta[name=viewport]");
+
+    // var myWidth = w < 500 ? 410 : 800;
+    // viewport.setAttribute('content', 'width=' + myWidth + ', maximum-scale=1.0, user-scalable=1');
+    // console.log(myWidth);
+
+    // var myViewport = w <= 412 ? viewport.setAttribute('content', 'width=1401, initial-scale=0.34, maximum-scale=1.0, user-scalable=1') : viewport.setAttribute('content', 'width=410, initial-scale=0.34, maximum-scale=1.0, user-scalable=1');
+
+    // else {
+        // viewport.setAttribute('content', 'width=640, initial-scale=0.47, maximum-scale=1.0, user-scalable=1');
+    // $(".desktop-mobile-view").html(changeViewportText); //this just gets the value that's already in it. maybe call function itself
+
     // w < 414 ? viewport.setAttribute("content", "width=device-width, initial-scale=1.0") : ;
 // viewport.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
-console.log("document width: " + w);
-    console.log(viewport);
+// console.log("document width: " + w);
+    // console.log("viewport: " + viewport);
     // event.preventDefault();
   });
+//   document.querySelector('#modal').addEventListener('click', function(e){
+//   e.stopPropagation();
+//   // carry on, nothing to see here
+// });
 
   // $(".rs-links").click(function(event){
   //   event.preventDefault();
