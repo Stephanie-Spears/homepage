6c9ef94462572e1cb2e89905b2147608135c5490
Stephanie Spears <stephanie.spears1@gmail.com>
2017-03-13 12:59:16 -0700

    update

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var myHomepage = function(){
 
   //"private" variables
   var viewport = document.querySelector("meta[name=viewport]");
-  console.log(viewport);
+
 
   var i = 0;
   var x = 0;
@@ -16,16 +16,16 @@ var myHomepage = function(){
     x = (x < gallerySource.length -1) ? ++x : 0;
     $("#homepage-main").css("background-image", gallerySource[x]).fadeOut(0).fadeIn(1000);
   };
-  var changeViewport = function(){
-    var w = $(document).width();
-    var text = w <= 414 ? "Desktop View" : "Mobile View";
-    $(".desktop-mobile-view").html(text);
-  };
+  // var changeViewport = function(){
+  //   var w = $(document).width();
+  //   var text = w <= 414 ? "Desktop View" : "Mobile View";
+  //   $(".desktop-mobile-view").html(text);
+  // };
 
 
   //front-end
   setInterval(function(){rotateGallery();},600000);
-  changeViewport();
+  // changeViewport();
   $("#nav-icon").click(function(){
     $("#nav-icon").toggleClass("open");
   });
@@ -52,8 +52,9 @@ var myHomepage = function(){
   });
 
   // With option
-$('body').previewer({
-  show: true
+$("body").previewer({
+  show: true,
+  type: "laptop"
 });
 
 // With method
